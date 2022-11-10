cc77b032cd1f3e207f31e4841491f80c31c19538
Jules Joseph <julesj93@gmail.com>
2022-11-10 17:11:30 -0500

    added get router to include models comment and user

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -47,3 +47,29 @@ router.get('/', withAuth, (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
