6602c8389d0ed5469e038e4b38296652d972f020
Jules Joseph <julesj93@gmail.com>
2022-11-10 17:33:18 -0500

    added additional parameters to Comment table for data base

--- models/Comment.js
+++ models/Comment.js
@@ -21,3 +21,28 @@ Comment.init({
             len: [1]
         }
     },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id'
+        }
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'post',
+            key: 'id'
+        }
+    }
+}, {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+})
+
+
+module.exports = Comment;
