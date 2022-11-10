6062cf2fad0b90415d4b738483a9bab575db5955
Jules Joseph <julesj93@gmail.com>
2022-11-10 17:32:50 -0500

    added const to comment table

---
+++ models/Comment.js
@@ -0,0 +1,23 @@
+const {
+    Model,
+    DataTypes
+} = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+class Comment extends Model {}
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    comment_text: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1]
+        }
+    },
