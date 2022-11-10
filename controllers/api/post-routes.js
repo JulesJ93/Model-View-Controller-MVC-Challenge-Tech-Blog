86a8eda38b12491026e3375bee50140c8fdb04b3
Jules Joseph <julesj93@gmail.com>
2022-11-10 16:50:40 -0500

    added then to catch errors in comments

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -28,3 +28,9 @@ router.get("/", (req, res) => {
                 },
             ],
         })
+        .then((dbPostData) => res.json(dbPostData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
