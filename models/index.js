43894c5a44fe7165ec6c7e5061c39b185ab8e6bb
Jules Joseph <julesj93@gmail.com>
2022-11-10 17:36:18 -0500

    added additional index.js

--- models/index.js
+++ models/index.js
@@ -18,3 +18,18 @@ Post.belongsTo(User, {
 Post.hasMany(Comment, {
     foreignKey: 'post_id'
 })
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+
+module.exports = {
+    User,
+    Post,
+    Comment
+};
