229ba7fa97c1f5fe632c0f6729c0018638414884
Jules Joseph <julesj93@gmail.com>
2022-11-10 17:23:59 -0500

    added get route to find id title content and created

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,3 +29,18 @@ router.get('/', (req, res) => {
                 }
             ]
         })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({
+                plain: true
+            }));
+
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
