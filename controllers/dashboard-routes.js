d5084cb6f56a5d5106741fa4229ec858db834fff
Jules Joseph <julesj93@gmail.com>
2022-11-10 17:16:25 -0500

    added get router and module exports

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -73,3 +73,33 @@ router.get('/edit/:id', withAuth, (req, res) => {
                 }
             ]
         })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({
+                    message: 'No post found with this id'
+                });
+                return;
+            }
+
+            const post = dbPostData.get({
+                plain: true
+            });
+
+            res.render('edit-post', {
+                post,
+                loggedIn: true
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+router.get('/new', (req, res) => {
+    res.render('add-post', {
+        loggedIn: true
+    })
+})
+
+module.exports = router;
