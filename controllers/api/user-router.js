d37f22ead4d54a2b4789209d8985be1ca7c19438
Jules Joseph <julesj93@gmail.com>
2022-11-10 17:01:52 -0500

    added .then for any errors and 404 status

--- controllers/api/user-router.js
+++ controllers/api/user-router.js
@@ -42,3 +42,17 @@ router.get('/:id', (req, res) => {
                 }
             ]
         })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({
+                    message: 'No user found with this id'
+                });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
